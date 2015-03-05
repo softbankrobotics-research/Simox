f1eb6f03e2846e67761adee29197d1cb83b8e7d8
p-kaiser <p-kaiser@042f3d55-54a8-47e9-b7fb-15903f145c44>
2015-03-05 12:57:35 +0000

    ConstrainedIK: Bugfix, throw exception if solve() is called on uninitialized IK instance
    git-svn-id: http://svn.code.sf.net/p/simox/code/trunk@791 042f3d55-54a8-47e9-b7fb-15903f145c44

--- VirtualRobot/IK/ConstrainedHierarchicalIK.cpp
+++ VirtualRobot/IK/ConstrainedHierarchicalIK.cpp
@@ -23,6 +23,8 @@ bool ConstrainedHierarchicalIK::initialize()
 
 bool ConstrainedHierarchicalIK::solveStep()
 {
+    THROW_VR_EXCEPTION_IF(!ik, "IK not initialized, did you forget to call initialize()?");
+
     Eigen::VectorXf jointValues;
     Eigen::VectorXf delta = ik->computeStep(jacobians);
 
--- VirtualRobot/IK/ConstrainedStackedIK.cpp
+++ VirtualRobot/IK/ConstrainedStackedIK.cpp
@@ -24,6 +24,8 @@ bool ConstrainedStackedIK::initialize()
 
 bool ConstrainedStackedIK::solveStep()
 {
+    THROW_VR_EXCEPTION_IF(!ik, "IK not initialized, did you forget to call initialize()?");
+
     Eigen::VectorXf jointValues;
     Eigen::VectorXf delta = ik->computeStep(jacobians);
 
