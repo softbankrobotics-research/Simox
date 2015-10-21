96d0ba0a9ffa2b7fd9072dd1f8d58d390773f9d5
Nikolaus Vahrenkamp <vahrenkamp@kit.edu>
2015-10-21 15:36:17 +0200

    version 2.3.15

--- config.cmake
+++ config.cmake
@@ -47,7 +47,7 @@ IF (NOT Simox_CONFIGURED)
 	############################# VERSION #################################
 	set(Simox_MAJOR_VERSION 2)
 	set(Simox_MINOR_VERSION 3)
-	set(Simox_PATCH_VERSION 14)
+        set(Simox_PATCH_VERSION 15)
 	set(Simox_VERSION
     ${Simox_MAJOR_VERSION}.${Simox_MINOR_VERSION}.${Simox_PATCH_VERSION})
 
