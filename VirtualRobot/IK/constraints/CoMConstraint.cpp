0e64979ba8c0e3fd22f4b4a7ad5bf710df4c992f
Peter Kaiser <peter.kaiser@kit.edu>
2016-06-06 14:00:18 +0200

    CoMConstraint: Initial version

---
+++ VirtualRobot/IK/constraints/CoMConstraint.cpp
@@ -0,0 +1,110 @@
+/**
+* This file is part of Simox.
+*
+* Simox is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version.
+*
+* Simox is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+* @package    VirtualRobot
+* @author     Peter Kaiser
+* @copyright  2016 Peter Kaiser
+*             GNU Lesser General Public License
+*
+*/
+
+#include "CoMConstraint.h"
+
+#include <VirtualRobot/Robot.h>
+
+using namespace VirtualRobot;
+
+CoMConstraint::CoMConstraint(const RobotPtr &robot, const RobotNodeSetPtr &joints, const RobotNodeSetPtr &bodies, const Eigen::Vector3f &target, float tolerance) :
+    Constraint(joints),
+    robot(robot),
+    nodeSetJoints(joints),
+    nodeSetBodies(bodies),
+    target(target),
+    dimensions(3),
+    tolerance(tolerance)
+{
+    ik.reset(new CoMIK(nodeSetJoints, nodeSetBodies, RobotNodePtr(), 3));
+    ik->setGoal(target);
+
+    addOptimizationFunction(0, false);
+}
+
+CoMConstraint::CoMConstraint(const RobotPtr &robot, const RobotNodeSetPtr &joints, const RobotNodeSetPtr &bodies, const Eigen::Vector2f &target, float tolerance) :
+    Constraint(joints),
+    robot(robot),
+    nodeSetJoints(joints),
+    nodeSetBodies(bodies),
+    target(target),
+    dimensions(2),
+    tolerance(tolerance)
+{
+    ik.reset(new CoMIK(nodeSetJoints, nodeSetBodies));
+    ik->setGoal(target);
+
+    addOptimizationFunction(0, false);
+}
+
+double CoMConstraint::optimizationFunction(unsigned int id)
+{
+    if(dimensions == 2)
+    {
+        Eigen::Vector2f d = nodeSetBodies->getCoM().head(2) - target;
+        return optimizationFunctionFactor * d.dot(d);
+    }
+    else if(dimensions == 3)
+    {
+        Eigen::Vector3f d = nodeSetBodies->getCoM() - target;
+        return optimizationFunctionFactor * d.dot(d);
+    }
+
+    return 0;
+}
+
+Eigen::VectorXf CoMConstraint::optimizationGradient(unsigned int id)
+{
+    int size = nodeSetJoints->getSize();
+
+    if(dimensions == 2)
+    {
+        Eigen::MatrixXf J = ik->getJacobianMatrix().block(0, 0, 2, size);
+        Eigen::Vector2f d = nodeSetBodies->getCoM().head(2) - target;
+        return 2 * optimizationFunctionFactor * d.transpose() * J;
+    }
+    else if(dimensions == 3)
+    {
+        Eigen::MatrixXf J = ik->getJacobianMatrix();
+        Eigen::Vector3f d = nodeSetBodies->getCoM() - target;
+        return 2 * optimizationFunctionFactor * d.transpose() * J;
+    }
+
+    return Eigen::VectorXf::Zero(size);
+}
+
+bool CoMConstraint::checkTolerances()
+{
+    if(dimensions == 2)
+    {
+        Eigen::Vector2f d = nodeSetBodies->getCoM().head(2) - target;
+        return d.norm() <= tolerance;
+    }
+    else if(dimensions == 3)
+    {
+        Eigen::Vector3f d = nodeSetBodies->getCoM() - target;
+        return d.norm() <= tolerance;
+    }
+
+    return 0;
+}
---
+++ VirtualRobot/IK/constraints/CoMConstraint.h
@@ -0,0 +1,60 @@
+/**
+* This file is part of Simox.
+*
+* Simox is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version.
+*
+* Simox is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+* @package    VirtualRobot
+* @author     Peter Kaiser
+* @copyright  2016 Peter Kaiser
+*             GNU Lesser General Public License
+*
+*/
+
+#ifndef _VirtualRobot_CoMConstraint_h_
+#define _VirtualRobot_CoMConstraint_h_
+
+#include "VirtualRobot/VirtualRobot.h"
+#include "VirtualRobot/IK/Constraint.h"
+#include "VirtualRobot/IK/CoMIK.h"
+
+#include <boost/shared_ptr.hpp>
+
+namespace VirtualRobot
+{
+    class VIRTUAL_ROBOT_IMPORT_EXPORT CoMConstraint : public Constraint, public boost::enable_shared_from_this<CoMConstraint>
+    {
+    public:
+        CoMConstraint(const RobotPtr& robot, const RobotNodeSetPtr& joints, const RobotNodeSetPtr& bodies, const Eigen::Vector3f& target, float tolerance);
+        CoMConstraint(const RobotPtr& robot, const RobotNodeSetPtr& joints, const RobotNodeSetPtr& bodies, const Eigen::Vector2f& target, float tolerance);
+
+        double optimizationFunction(unsigned int id);
+        Eigen::VectorXf optimizationGradient(unsigned int id);
+        bool checkTolerances();
+
+    protected:
+        RobotPtr robot;
+        RobotNodeSetPtr nodeSetJoints;
+        RobotNodeSetPtr nodeSetBodies;
+        Eigen::VectorXf target;
+
+        CoMIKPtr ik;
+        int dimensions;
+        float tolerance;
+    };
+
+    typedef boost::shared_ptr<CoMConstraint> CoMConstraintPtr;
+}
+
+#endif
+
