c7d09bf9375ff5edb999968234c1f980b72f1f95
Adrian Knobloch <adrian.knobloch@student.kit.edu>
2016-08-13 20:22:29 +0200

    Implement fixed joint.

--- VirtualRobot/Nodes/ModelJointFixed.cpp
+++ VirtualRobot/Nodes/ModelJointFixed.cpp
@@ -1,1 +1,48 @@
 #include "ModelJointFixed.h"
+
+namespace VirtualRobot
+{
+    ModelJointFixed::ModelJointFixed(ModelWeakPtr model, const std::string& name, Eigen::Matrix4f& localTransformation)
+            : ModelJoint(model, name, localTransformation, 0, 0, 0.0f)
+    {
+    }
+
+    ModelJointFixed::~ModelJointFixed()
+    {
+    }
+
+    ModelNode::ModelNodeType ModelJointFixed::getType() const
+    {
+        return ModelNode::ModelNodeType::JointFixed;
+    }
+
+    void ModelJointFixed::setJointValue(float)
+    {
+        // do nothing
+    }
+
+    void ModelJointFixed::setJointValueNoUpdate(float)
+    {
+        // do nothing
+    }
+
+    void ModelJointFixed::setJointLimits(float, float)
+    {
+        // do nothing
+    }
+
+    void ModelJointFixed::setMaxVelocity(float)
+    {
+        // do nothing
+    }
+
+    void ModelJointFixed::setMaxAcceleration(float)
+    {
+        // do nothing
+    }
+
+    void ModelJointFixed::setMaxTorque(float)
+    {
+        // do nothing
+    }
+}
