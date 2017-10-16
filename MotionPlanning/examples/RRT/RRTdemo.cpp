648155d2e734d859d48b41afb9da7711268caffa
Nikolaus Vahrenkamp <vahrenkamp@kit.edu>
2017-10-16 13:42:50 +0200

    simox gui for RRTdemo

--- MotionPlanning/examples/RRT/RRTdemo.cpp
+++ MotionPlanning/examples/RRT/RRTdemo.cpp
@@ -5,18 +5,15 @@
 #include "VirtualRobot/Model/Nodes/ModelNode.h"
 #include "VirtualRobot/XML/ModelIO.h"
 #include "VirtualRobot/Visualization/VisualizationFactory.h"
-#include "VirtualRobot/Visualization/CoinVisualization/CoinVisualization.h"
-#include "VirtualRobot/Visualization/CoinVisualization/CoinVisualizationFactory.h"
 #include "MotionPlanning/MotionPlanning.h"
 #include "MotionPlanning/Planner/Rrt.h"
 #include "MotionPlanning/Planner/BiRrt.h"
-#include "MotionPlanning/Visualization/CoinVisualization/CoinRrtWorkspaceVisualization.h"
-
-#include <Inventor/Qt/viewers/SoQtExaminerViewer.h>
-#include <Inventor/nodes/SoSeparator.h>
-#include <Inventor/Qt/SoQt.h>
+#include "MotionPlanning/Visualization/RrtWorkspaceVisualization.h"
 
 #include <VirtualRobot/Import/SimoxXMLFactory.h>
+
+#include "../../../Gui/ViewerInterface.h"
+#include "../../../Gui/ViewerFactory.h"
 
 #include "string"
 #include <iostream>
@@ -26,12 +23,22 @@ using std::endl;
 using namespace VirtualRobot;
 using namespace MotionPlanning;
 
+
+#ifdef Simox_USE_COIN_VISUALIZATION
+    #include "../../../Gui/Coin/CoinViewerFactory.h"
+    // need this to ensure that static Factory methods are called across library boundaries (otherwise coin Gui lib is not loaded since it is not referenced by us)
+    SimoxGui::CoinViewerFactory f;
+
+    #include "MotionPlanning/Visualization/CoinVisualization/CoinRrtWorkspaceVisualization.h"
+#endif
+
+
 #define USE_BIRRT
 
 bool useColModel = false;
 QWidget* win;
 
-void show(SoNode* n)
+void show(std::vector<VisualizationPtr> &visus)
 {
     if (win == NULL)
     {
@@ -39,36 +46,28 @@ void show(SoNode* n)
         exit(-3);
     }
 
-    SoQtExaminerViewer* viewer = new SoQtExaminerViewer(win);
+    SimoxGui::ViewerInterfacePtr viewer;
+    SimoxGui::ViewerFactoryPtr viewerFactory = SimoxGui::ViewerFactory::first(NULL);
+    THROW_VR_EXCEPTION_IF(!viewerFactory,"No viewer factory?!");
+    viewer = viewerFactory->createViewer(win);
 
-    // set the robot
-    if (n)
+    // set the visu
+    int i = 0;
+    for (auto &n: visus)
     {
-        viewer->setSceneGraph(n);
+        std::stringstream vn;
+        vn << "rrt-" << i;
+        viewer->addVisualization("rrt", vn.str(), n);
+        i++;
     }
 
-    // register timer callback for animation and draw state updates
-    /*
-    SoSensorManager *sensor_mgr = SoDB::getSensorManager();
-    SoTimerSensor *timer = new SoTimerSensor(TimerCallback, NULL);
-    timer->setInterval(SbTime(0.02));
-    sensor_mgr->insertTimerSensor(timer);
-    */
-    viewer->setBackgroundColor(SbColor(1.0f, 1.0f, 1.0f));
-    viewer->setAccumulationBuffer(true);
-    viewer->setAntialiasing(true, 4);
-    viewer->setTransparencyType(SoGLRenderAction::SORTED_OBJECT_BLEND);
-    viewer->setFeedbackVisibility(true);
-
     // show everything
-    viewer->show();
+    viewer->viewAll();
 
-    // start the mainloop
-    SoQt::show(win);
-    SoQt::mainLoop();
+    viewer->start(win);
 
     // clean up
-    delete viewer;
+    viewer.reset();
 }
 
 void startRRTVisualization()
@@ -94,7 +93,6 @@ void startRRTVisualization()
 
     Eigen::Affine3f tmpT(Eigen::Translation3f(posX, posY, posZ));
     o->setGlobalPose(tmpT.matrix());
-
 
     // setup collision detection
     std::string colModelName("CollisionModel");
@@ -169,41 +167,48 @@ void startRRTVisualization()
     CSpacePathPtr solution = rrt->getSolution();
     CSpaceTreePtr tree = rrt->getTree();
 
-
-
     planningNodes->setJointValues(start);
 
     // display robot
-    SoSeparator* sep = new SoSeparator();
     ModelLink::VisualizationType colModel = ModelLink::VisualizationType::Full;
 
-    SoNode* visualisationNode = CoinVisualizationFactory::getCoinVisualization(robot, colModel);
-    sep->addChild(visualisationNode);
+    VisualizationPtr visualisationRobot = VisualizationFactory::getGlobalVisualizationFactory()->createVisualization(robot, colModel);
+
 
     // display obstacle
-    SoNode* visualisationNodeO = CoinVisualizationFactory::getCoinVisualization(o, colModel);
-    sep->addChild(visualisationNodeO);
+    VisualizationPtr visualisationObstacle = VisualizationFactory::getGlobalVisualizationFactory()->createVisualization(o, colModel);
 
     // show rrt visu
-    std::shared_ptr<CoinRrtWorkspaceVisualization> w(new CoinRrtWorkspaceVisualization(robot, cspace, "EndPoint"));
+
+    RrtWorkspaceVisualizationPtr w;
+#ifdef Simox_USE_COIN_VISUALIZATION
+    w.reset(new CoinRrtWorkspaceVisualization(robot, cspace, "EndPoint"));
+#endif
+
     w->addTree(tree);
 #ifdef USE_BIRRT
     CSpaceTreePtr tree2 = rrt->getTree2();
     w->addTree(tree2);
 #endif
     w->addCSpacePath(solution);
-    w->addConfiguration(start, CoinRrtWorkspaceVisualization::eGreen, 3.0f);
-    w->addConfiguration(goal, CoinRrtWorkspaceVisualization::eGreen, 3.0f);
-    SoSeparator* sol = w->getCoinVisualization();
-    sep->addChild(sol);
+    w->addConfiguration(start, RrtWorkspaceVisualization::eGreen, 3.0f);
+    w->addConfiguration(goal, RrtWorkspaceVisualization::eGreen, 3.0f);
 
-    show(sep);
+    VisualizationPtr visuRrt = w->getVisualization();
+
+    std::vector<VisualizationPtr> visus;
+    visus.push_back(visualisationRobot);
+    visus.push_back(visualisationObstacle);
+    visus.push_back(visuRrt);
+
+    show(visus);
 }
 
-int main(int /*argc*/, char** /*argv*/)
+int main(int argc, char** argv)
 {
-    SoDB::init();
-    win = SoQt::init("RRT Demo", "RRT Demo");
+    VirtualRobot::init(argc, argv, "RRT-Demo");
+    win = new QWidget();
+    win->resize(640,480);
     cout << " --- START --- " << endl;
 
     try
