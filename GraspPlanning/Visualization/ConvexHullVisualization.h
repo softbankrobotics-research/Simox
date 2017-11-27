871ab01621ab161f1a5ce7e419e0de92cfec556c
Adrian Knobloch <adrian.knobloch@student.kit.edu>
2017-11-27 10:55:43 +0100

    Remove CoinConvexHullVisualization and implement the functionality using the VisualizationFactory

--- GraspPlanning/Visualization/ConvexHullVisualization.h
+++ GraspPlanning/Visualization/ConvexHullVisualization.h
@@ -49,17 +49,19 @@ namespace GraspPlanning
         ConvexHullVisualization(VirtualRobot::MathTools::ConvexHull6DPtr convHull, bool useFirst3Coords = true);
         ConvexHullVisualization(VirtualRobot::MathTools::ConvexHull3DPtr convHull);
 
-        virtual VirtualRobot::VisualizationSetPtr getVisualization() = 0;
+        ~ConvexHullVisualization() = default;
 
-        /*!
-        */
-        virtual ~ConvexHullVisualization();
+        virtual VirtualRobot::VisualizationPtr getVisualization();
 
     protected:
+        static VirtualRobot::VisualizationPtr createConvexHullVisualization(const VirtualRobot::MathTools::ConvexHull3DPtr& convHull);
+        static VirtualRobot::VisualizationPtr createConvexHullVisualization(const VirtualRobot::MathTools::ConvexHull6DPtr& convHull, bool buseFirst3Coords);
 
         VirtualRobot::MathTools::ConvexHull3DPtr convHull3D;
         VirtualRobot::MathTools::ConvexHull6DPtr convHull6D;
         bool useFirst3Coords;
+
+        VirtualRobot::VisualizationPtr visualization;
     };
 
     typedef std::shared_ptr<ConvexHullVisualization> ConvexHullVisualizationPtr;
