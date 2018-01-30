c175f2e373b019065c0daa17b1ee15a75281d4ca
Raphael Grimm <raphael.grimm@kit.edu>
2018-01-30 09:41:04 +0100

    v2.3.60

--- config.cmake
+++ config.cmake
@@ -50,7 +50,7 @@ IF (NOT Simox_CONFIGURED)
     ############################# VERSION #################################
     set(Simox_MAJOR_VERSION 2)
     set(Simox_MINOR_VERSION 3)
-    set(Simox_PATCH_VERSION 59)
+    set(Simox_PATCH_VERSION 60)
     set(Simox_VERSION
     ${Simox_MAJOR_VERSION}.${Simox_MINOR_VERSION}.${Simox_PATCH_VERSION})
 
