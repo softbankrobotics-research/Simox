01721789cbc734fa633478d008f8fdf5641f46dd
Simon Ottenhaus <simon.ottenhaus@kit.edu>
2018-05-14 17:24:43 +0200

    header comments

--- VirtualRobot/math/Grid3D.h
+++ VirtualRobot/math/Grid3D.h
@@ -13,7 +13,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
-* @author     miller (martin dot miller at student dot kit dot edu)
+* @author     Simon Ottenhaus (simon dot ottenhaus at kit dot edu)
 * @copyright  http://www.gnu.org/licenses/gpl-2.0.txt
 *             GNU General Public License
 */
