3b225f6ac4ec382d840a0aeee3283af64eb8feea
Simon Ottenhaus <simon.ottenhaus@kit.edu>
2018-06-28 15:53:47 +0200

    added test

--- VirtualRobot/tests/MathGaussianImplicitSurface3DTest.cpp
+++ VirtualRobot/tests/MathGaussianImplicitSurface3DTest.cpp
@@ -44,5 +44,29 @@ BOOST_AUTO_TEST_CASE(testGPIS)
 
 }
 
+BOOST_AUTO_TEST_CASE(testGPISNoise)
+{
+    std::vector<DataR3R2> samples;
+    samples.push_back(DataR3R2(0, 0, 0, 1, 0.1f));
+    samples.push_back(DataR3R2(1, 0, 0, 0, 0.1f));
+    samples.push_back(DataR3R2(1, 0, 0, 1, 0.1f));
+
+    GaussianImplicitSurface3D gpis(KernelWithDerivatives::Create(KernelType::Gauss));
+    gpis.Calculate(samples);
+    //std::cout << gpis.Get(Vec3(0,0,0)) << std::endl;
+    //std::cout << gpis.Get(Vec3(1,0,0)) << std::endl;
+
+    BOOST_CHECK_LE(fabs(1 - gpis.Get(Vec3(0,0,0))), 0.1);
+    BOOST_CHECK_LE(fabs(0.5 - gpis.Get(Vec3(1,0,0))), 0.1);
+
+    /*for(const DataR3R2& s : samples)
+    {
+        float err = fabs(s.Value1() - gpis.Get(s.Position()));
+        BOOST_CHECK_LE(err, 0.1);
+        //std::cout << err << " " << s.Value1() << " " << gpis.Get(s.Position()) << std::endl;
+    }*/
+
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
