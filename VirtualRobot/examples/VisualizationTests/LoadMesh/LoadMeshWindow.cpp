f0840adede321a0a77818be782c27aeb69823e0a
Philipp Schmidt <ufedv@student.kit.edu>
2018-07-15 21:15:45 +0200

    Adapt LoadMesh example to scaling changes

--- VirtualRobot/examples/VisualizationTests/LoadMesh/LoadMeshWindow.cpp
+++ VirtualRobot/examples/VisualizationTests/LoadMesh/LoadMeshWindow.cpp
@@ -65,39 +65,47 @@ void LoadMeshWindow::setupUI()
     VirtualRobot::VisualizationPtr visuX = VirtualRobot::VisualizationFactory::getInstance()->createBox(0.02f, 0.02f, 0.02f);
     visuX->setGlobalPose(xmat);
     visuX->setColor(VirtualRobot::Visualization::Color(1.0f, 0.0f, 0.0f));
+    visuX->scale(1000.0f);
     viewer->addVisualization("axis", visuX);
 
     VirtualRobot::VisualizationPtr visuXaxis = VirtualRobot::VisualizationFactory::getInstance()->createBox(1.0f, 0.002f, 0.002f);
     visuXaxis->setGlobalPose(xmataxis);
     visuXaxis->setColor(VirtualRobot::Visualization::Color(1.0f, 0.0f, 0.0f));
+    visuXaxis->scale(1000.0f);
     viewer->addVisualization("axis", visuXaxis);
 
     VirtualRobot::VisualizationPtr visuY = VirtualRobot::VisualizationFactory::getInstance()->createBox(0.02f, 0.02f, 0.02f);
     visuY->setGlobalPose(ymat);
     visuY->setColor(VirtualRobot::Visualization::Color(0.0f, 1.0f, 0.0f));
+    visuY->scale(1000.0f);
     viewer->addVisualization("axis", visuY);
 
     VirtualRobot::VisualizationPtr visuYaxis = VirtualRobot::VisualizationFactory::getInstance()->createBox(0.002f, 1.0f, 0.002f);
     visuYaxis->setGlobalPose(ymataxis);
     visuYaxis->setColor(VirtualRobot::Visualization::Color(0.0f, 1.0f, 0.0f));
+    visuYaxis->scale(1000.0f);
     viewer->addVisualization("axis", visuYaxis);
 
     VirtualRobot::VisualizationPtr visuZ = VirtualRobot::VisualizationFactory::getInstance()->createBox(0.02f, 0.02f, 0.02f);
     visuZ->setGlobalPose(zmat);
     visuZ->setColor(VirtualRobot::Visualization::Color(0.0f, 0.0f, 1.0f));
+    visuZ->scale(1000.0f);
     viewer->addVisualization("axis", visuZ);
 
     VirtualRobot::VisualizationPtr visuZaxis = VirtualRobot::VisualizationFactory::getInstance()->createBox(0.002f, 0.002f, 1.0f);
     visuZaxis->setGlobalPose(zmataxis);
     visuZaxis->setColor(VirtualRobot::Visualization::Color(0.0f, 0.0f, 1.0f));
+    visuZaxis->scale(1000.0f);
     viewer->addVisualization("axis", visuZaxis);
 
 
 
     VirtualRobot::VisualizationPtr visu1 = VirtualRobot::VisualizationFactory::getInstance()->createVisualizationFromFile("/home/philipp/simox/data/robots/Armar4/urdf/stl/ArmL_Elb1_joint_visu.stl");
+    visu1->scale(1000.0f);
     viewer->addVisualization("test", visu1);
 
     VirtualRobot::VisualizationPtr visu2 = VirtualRobot::VisualizationFactory::getInstance()->createVisualizationFromFile("/home/philipp/simox/data/robots/Armar4/urdf/stl/ArmL_Elb1_joint_visu.dae");
+    visu2->scale(1000.0f);
     viewer->addVisualization("test", visu2);
 
     /*VirtualRobot::VisualizationPtr visu3 = VirtualRobot::VisualizationFactory::getInstance()->createVisualizationFromFile("/home/philipp/simox/data/robots/Armar4/urdf/stl/ArmL_Elb1_joint_visu.obj");
