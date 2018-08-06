47a24ab0d64fe29e021dd8b02bc04136d5970eeb
Philipp Schmidt <ufedv@student.kit.edu>
2018-08-06 05:08:17 +0200

    Add scaling convenience method

--- VirtualRobot/Visualization/Qt3DVisualization/Qt3DVisualization.h
+++ VirtualRobot/Visualization/Qt3DVisualization/Qt3DVisualization.h
@@ -57,6 +57,7 @@ namespace VirtualRobot
         virtual bool isSelected() const override;
         virtual size_t addSelectionChangedCallback(std::function<void (bool)> f) override;
         virtual void removeSelectionChangedCallback(size_t id) override;
+        inline void scale(float scaleFactor) {scale(Eigen::Vector3f(scaleFactor, scaleFactor, scaleFactor));}
         virtual void scale(const Eigen::Vector3f &scaleFactor) override;
         virtual void shrinkFatten(float offset) override;
         virtual bool hasManipulator(ManipulatorType t) const override;
