fc73684c6748a77af18a02e10f4931f2e0ddd8f6
Philipp Schmidt <ufedv@student.kit.edu>
2018-08-08 09:30:00 +0200

    Implement getTriMeshModel()
    First iteration, mergeVertices() in TriMesh must be disabled, takes way too long for large models

--- VirtualRobot/Visualization/Qt3DVisualization/Qt3DVisualization.cpp
+++ VirtualRobot/Visualization/Qt3DVisualization/Qt3DVisualization.cpp
@@ -238,8 +238,88 @@ namespace VirtualRobot
 
     TriMeshModelPtr Qt3DVisualization::getTriMeshModel() const
     {
-        std::cout << "getTriMeshModel()" << std::endl;
-        return TriMeshModelPtr(new TriMeshModel());
+        TriMeshModelPtr triMesh(new TriMeshModel());
+        unsigned int globalIndexCounter = 0;
+
+        for(Qt3DRender::QGeometryRenderer* render : this->entity->findChildren<Qt3DRender::QGeometryRenderer*>())
+        {
+            if(render->primitiveType() == Qt3DRender::QGeometryRenderer::PrimitiveType::Triangles)
+            {
+                Qt3DRender::QAttribute *vertexAttribute = nullptr;
+                QByteArray vertexData;
+                Qt3DRender::QAttribute *normalAttribute = nullptr;
+                QByteArray normalData;
+                Qt3DRender::QAttribute *indexAttribute = nullptr;
+                QByteArray indexData;
+
+                for (Qt3DRender::QAttribute *attribute : render->geometry()->attributes())
+                {
+                    auto buffer = attribute->buffer();
+                    auto generator = attribute->buffer()->dataGenerator();
+                    auto name = attribute->name();
+                    auto type = attribute->attributeType();
+
+                    if(type == Qt3DRender::QAttribute::VertexAttribute)
+                    {
+                        if(name.compare(Qt3DRender::QAttribute::defaultPositionAttributeName()) == 0)
+                        {
+                            if (generator)
+                                vertexData = (*generator.data())();
+                            else
+                                vertexData = buffer->data();
+                            vertexAttribute = attribute;
+                        }
+                        else if(name.compare(Qt3DRender::QAttribute::defaultNormalAttributeName()) == 0)
+                        {
+                            if (generator)
+                                normalData = (*generator.data())();
+                            else
+                                normalData = buffer->data();
+                            normalAttribute = attribute;
+                        }
+                    }
+                    else if(type == Qt3DRender::QAttribute::IndexAttribute)
+                    {
+                        if (generator)
+                            indexData = (*generator.data())();
+                        else
+                            indexData = buffer->data();
+                        indexAttribute = attribute;
+                    }
+                }
+
+                if(vertexAttribute && normalAttribute && indexAttribute)
+                {
+                    if(indexAttribute->vertexBaseType() == Qt3DRender::QAttribute::UnsignedShort)
+                    {
+                        ushort *rawIndexArray = reinterpret_cast<ushort *>(indexData.data());
+                        float *rawVertexArray = reinterpret_cast<float *>(vertexData.data());
+                        float *rawNormalArray = reinterpret_cast<float *>(normalData.data());
+
+                        for(unsigned int i = vertexAttribute->byteOffset(); i < vertexAttribute->count() * (vertexAttribute->byteStride() / sizeof(float)); i += (vertexAttribute->byteStride() / sizeof(float)))
+                        {
+                            triMesh->addVertex(MathTools::transformPosition(Eigen::Vector3f(rawVertexArray[i], rawVertexArray[i + 1], rawVertexArray[i + 2]), scaleMatrix));
+                        }
+                        for(unsigned int i = normalAttribute->byteOffset(); i < normalAttribute->count() * (normalAttribute->byteStride() / sizeof(float)); i += (normalAttribute->byteStride() / sizeof(float)))
+                        {
+                            //triMesh->addNormal(MathTools::transformPosition(Eigen::Vector3f(rawNormalArray[i], rawNormalArray[i + 1], rawNormalArray[i + 2]), scaleMatrix).normalized());
+                            triMesh->addNormal(Eigen::Vector3f(rawNormalArray[i], rawNormalArray[i + 1], rawNormalArray[i + 2]));
+                        }
+                        for(unsigned int i = 0; i < indexAttribute->count(); i += 3)
+                        {
+                            triMesh->addFace(globalIndexCounter + rawIndexArray[i], globalIndexCounter + rawIndexArray[i + 1], globalIndexCounter + rawIndexArray[i + 2]);
+                        }
+                        globalIndexCounter += vertexAttribute->count();
+                    }
+                    else if(indexAttribute->vertexBaseType() == Qt3DRender::QAttribute::UnsignedInt)
+                    {
+                        std::cout << "Unsigned Integer Index Buffer detected" << std::endl;
+                    }
+                }
+            }
+        }
+
+        return triMesh;
     }
 
     int Qt3DVisualization::getNumFaces() const
