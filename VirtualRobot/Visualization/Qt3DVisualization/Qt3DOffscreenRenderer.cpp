89cb3a69bacf26170d1a8183f249b00e96efb1c6
Philipp Schmidt <ufedv@student.kit.edu>
2018-10-22 09:51:47 +0200

    Add offscreen engine

--- VirtualRobot/Visualization/Qt3DVisualization/Qt3DOffscreenRenderer.cpp
+++ VirtualRobot/Visualization/Qt3DVisualization/Qt3DOffscreenRenderer.cpp
@@ -24,4 +24,176 @@ namespace VirtualRobot
     {
         return "qt3d";
     }
+
+    Qt3DOffscreenRenderer::TextureRenderTarget::TextureRenderTarget(Qt3DCore::QNode *parent,
+                                             const QSize &size,
+                                             Qt3DRender::QRenderTargetOutput::AttachmentPoint attatchmentPoint) :
+        Qt3DRender::QRenderTarget(parent),
+        size(size)
+    {
+        // The lifetime of the objects created here is managed
+        // automatically, as they become children of this object.
+
+        // Create a render target output for rendering colour.
+        output = new Qt3DRender::QRenderTargetOutput(this);
+        output->setAttachmentPoint(attatchmentPoint);
+
+        // Create a texture to render into.
+        texture = new Qt3DRender::QTexture2D(output);
+        texture->setSize(size.width(), size.height());
+        texture->setFormat(Qt3DRender::QAbstractTexture::RGB8_UNorm);
+        texture->setMinificationFilter(Qt3DRender::QAbstractTexture::Linear);
+        texture->setMagnificationFilter(Qt3DRender::QAbstractTexture::Linear);
+
+        // Hook the texture up to our output, and the output up to this object.
+        output->setTexture(texture);
+        addOutput(output);
+
+        depthTextureOutput = new Qt3DRender::QRenderTargetOutput(this);
+        depthTextureOutput->setAttachmentPoint(Qt3DRender::QRenderTargetOutput::Depth);
+        depthTexture = new Qt3DRender::QTexture2D(depthTextureOutput);
+        depthTexture->setSize(size.width(), size.height());
+        depthTexture->setFormat(Qt3DRender::QAbstractTexture::DepthFormat);
+        depthTexture->setMinificationFilter(Qt3DRender::QAbstractTexture::Linear);
+        depthTexture->setMagnificationFilter(Qt3DRender::QAbstractTexture::Linear);
+        depthTexture->setComparisonFunction(Qt3DRender::QAbstractTexture::CompareLessEqual);
+        depthTexture->setComparisonMode(Qt3DRender::QAbstractTexture::CompareRefToTexture);
+        // Hook up the depth texture
+        depthTextureOutput->setTexture(depthTexture);
+        addOutput(depthTextureOutput);
+    }
+
+    void Qt3DOffscreenRenderer::TextureRenderTarget::setSize(const QSize &size)
+    {
+        this->size = size;
+        texture->setSize(size.width(), size.height());
+    }
+
+    Qt3DRender::QTexture2D* Qt3DOffscreenRenderer::TextureRenderTarget::getTexture()
+    {
+        return texture;
+    }
+
+
+
+    Qt3DOffscreenRenderer::OffscreenSurfaceFrameGraph::OffscreenSurfaceFrameGraph(Qt3DCore::QNode* parent, Qt3DRender::QCamera *camera, const QSize &size) :
+        Qt3DRender::QRenderSurfaceSelector(parent),
+        camera(camera)
+    {
+        // Firstly, create the offscreen surface. This will take the place
+        // of a QWindow, allowing us to render our scene without one.
+        offscreenSurface = new QOffscreenSurface();
+        offscreenSurface->setFormat(QSurfaceFormat::defaultFormat());
+        offscreenSurface->create();
+
+        // Hook it up to the frame graph.
+        setSurface(offscreenSurface);
+        setExternalRenderTargetSize(size);
+
+        // Create a texture to render into. This acts as the buffer that
+        // holds the rendered image.
+        renderTargetSelector = new Qt3DRender::QRenderTargetSelector(this);
+        textureTarget = new TextureRenderTarget(renderTargetSelector, size);
+        renderTargetSelector->setTarget(textureTarget);
+
+        // Create a node used for clearing the required buffers.
+        clearBuffers = new Qt3DRender::QClearBuffers(renderTargetSelector);
+        clearBuffers->setClearColor(QColor(100, 100, 100, 255));
+        clearBuffers->setBuffers(Qt3DRender::QClearBuffers::ColorDepthBuffer);
+
+        // Create a viewport node. The viewport here just covers the entire render area.
+        viewport = new Qt3DRender::QViewport(renderTargetSelector);
+        viewport->setNormalizedRect(QRectF(0.0, 0.0, 1.0, 1.0));
+
+        // Create a camera selector node, and tell it to use the camera we've ben given.
+        cameraSelector = new Qt3DRender::QCameraSelector(viewport);
+        cameraSelector->setCamera(camera);
+    }
+
+    void Qt3DOffscreenRenderer::OffscreenSurfaceFrameGraph::setSize(const QSize &size)
+    {
+        textureTarget->setSize(size);
+        setExternalRenderTargetSize(size);
+    }
+
+    Qt3DCore::QNode *Qt3DOffscreenRenderer::OffscreenSurfaceFrameGraph::getRenderTargetSelector()
+    {
+        return renderTargetSelector;
+    }
+
+    Qt3DOffscreenRenderer::OffscreenEngine::OffscreenEngine(Qt3DRender::QCamera *camera, const QSize &size)
+    {
+        sceneRoot = nullptr;
+
+        // Set up the engine and the aspects that we want to use.
+        aspectEngine = new Qt3DCore::QAspectEngine();
+        renderAspect = new Qt3DRender::QRenderAspect(Qt3DRender::QRenderAspect::Threaded); // Only threaded mode seems to work right now.
+        logicAspect = new Qt3DLogic::QLogicAspect();
+
+        aspectEngine->registerAspect(renderAspect);
+        aspectEngine->registerAspect(logicAspect);
+
+        // Create the root entity of the engine.
+        // This is not the same as the 3D scene root: the QRenderSettings
+        // component must be held by the root of the QEntity tree,
+        // so it is added to this one. The 3D scene is added as a subtree later,
+        // in setSceneRoot().
+        Qt3DCore::QEntityPtr root(new Qt3DCore::QEntity());
+        renderSettings = new Qt3DRender::QRenderSettings(root.data());
+        root->addComponent(renderSettings);
+
+        // Create the offscreen frame graph, which will manage all of the resources required
+        // for rendering without a QWindow.
+        offscreenFrameGraph = new OffscreenSurfaceFrameGraph(renderSettings, camera, size);
+
+        // Set this frame graph to be in use.
+        renderSettings->setActiveFrameGraph(offscreenFrameGraph);
+
+        // Add a render capture node to the frame graph.
+        // This is set as the next child of the render target selector node,
+        // so that the capture will be taken from the specified render target
+        // once all other rendering operations have taken place.
+        renderCapture = new Qt3DRender::QRenderCapture(offscreenFrameGraph->getRenderTargetSelector());
+
+        // Set the root entity of the engine. This causes the engine to begin running.
+        aspectEngine->setRootEntity(root);
+    }
+
+    Qt3DOffscreenRenderer::OffscreenEngine::~OffscreenEngine()
+    {
+        // Setting a null root entity shuts down the engine.
+        aspectEngine->setRootEntity(Qt3DCore::QEntityPtr());
+
+        // Not sure if the following is strictly required, as it may
+        // happen automatically when the engine is destroyed.
+        aspectEngine->unregisterAspect(logicAspect);
+        aspectEngine->unregisterAspect(renderAspect);
+        delete logicAspect;
+        delete renderAspect;
+
+        delete aspectEngine;
+    }
+
+    void Qt3DOffscreenRenderer::OffscreenEngine::setSceneRoot(Qt3DCore::QNode *sceneRoot)
+    {
+        // Make sure any existing scene root is unparented.
+        if ( this->sceneRoot )
+        {
+            this->sceneRoot->setParent(static_cast<Qt3DCore::QNode*>(nullptr));
+        }
+
+        // Parent the incoming scene root to our current root entity.
+        this->sceneRoot = sceneRoot;
+        this->sceneRoot->setParent(aspectEngine->rootEntity().data());
+    }
+
+    Qt3DRender::QRenderCapture* Qt3DOffscreenRenderer::OffscreenEngine::getRenderCapture()
+    {
+        return renderCapture;
+    }
+
+    void Qt3DOffscreenRenderer::OffscreenEngine::setSize(const QSize &size)
+    {
+        offscreenFrameGraph->setSize(size);
+    }
 }
--- VirtualRobot/Visualization/Qt3DVisualization/Qt3DOffscreenRenderer.h
+++ VirtualRobot/Visualization/Qt3DVisualization/Qt3DOffscreenRenderer.h
@@ -25,6 +25,26 @@
 
 #include "../OffscreenRenderer.h"
 
+
+#include <Qt3DRender/QRenderTarget>
+#include <Qt3DRender/QRenderTargetOutput>
+#include <Qt3DRender/QTexture>
+#include <QOffscreenSurface>
+#include <Qt3DRender/QRenderSurfaceSelector>
+#include <Qt3DRender/QRenderTargetSelector>
+#include <Qt3DRender/QViewport>
+#include <Qt3DRender/QClearBuffers>
+#include <Qt3DRender/QCameraSelector>
+#include <Qt3DRender/QCamera>
+#include <Qt3DCore/QEntity>
+#include <Qt3DCore/QNode>
+#include <Qt3DRender/QRenderSettings>
+#include <Qt3DRender/QRenderCapture>
+#include <Qt3DCore/QAspectEngine>
+#include <Qt3DRender/QRenderAspect>
+#include <Qt3DLogic/QLogicAspect>
+
+
 namespace VirtualRobot
 {
     class VIRTUAL_ROBOT_IMPORT_EXPORT Qt3DOffscreenRenderer  : public OffscreenRenderer
@@ -53,6 +73,75 @@ namespace VirtualRobot
          * @return the visualization type that is supported by this factory.
          */
         virtual std::string getVisualizationType() const override;
+
+    private:
+        // Encapsulates a 2D texture that a frame graph can render into.
+        class TextureRenderTarget : public Qt3DRender::QRenderTarget
+        {
+        public:
+            TextureRenderTarget(Qt3DCore::QNode *parent = nullptr,
+                                const QSize &size = QSize(500, 500),
+                                Qt3DRender::QRenderTargetOutput::AttachmentPoint attatchmentPoint
+                                                        = Qt3DRender::QRenderTargetOutput::Color0);
+
+            void setSize(const QSize &size);
+            QSize getSize() { return size; }
+            Qt3DRender::QTexture2D* getTexture();
+
+        private:
+            QSize size;
+            Qt3DRender::QRenderTargetOutput *output;
+            Qt3DRender::QTexture2D *texture;
+            // To enable depth testing
+            Qt3DRender::QRenderTargetOutput *depthTextureOutput;
+            Qt3DRender::QTexture2D *depthTexture;
+        };
+
+        // The OffscreenSurfaceFrameGraph class is where the magic happens.
+        // It is responsible for rendering the scene to an "offscreen" surface (ie. a texture),
+        // rather than directly to a QWindow. This means that the render contents can be
+        // taken and used within other QWidgets.
+        class OffscreenSurfaceFrameGraph : public Qt3DRender::QRenderSurfaceSelector
+        {
+        public:
+            OffscreenSurfaceFrameGraph(Qt3DCore::QNode* parent = nullptr, Qt3DRender::QCamera *camera = nullptr, const QSize &size = QSize(500, 500));
+
+            void setSize(const QSize &size);
+            Qt3DCore::QNode *getRenderTargetSelector();
+
+        private:
+            TextureRenderTarget *textureTarget;
+            QOffscreenSurface *offscreenSurface;
+            Qt3DRender::QRenderTargetSelector *renderTargetSelector;
+            Qt3DRender::QViewport *viewport;
+            Qt3DRender::QClearBuffers *clearBuffers;
+            Qt3DRender::QCameraSelector *cameraSelector;
+            Qt3DRender::QCamera *camera;
+        };
+
+        // The OffscreenEngine brings together various Qt3D classes that are required in order to
+        // perform basic scene rendering. Of these, the most important for this project is the OffscreenSurfaceFrameGraph.
+        // Render captures can be requested, and the capture contents used within other widgets (see OffscreenEngineDelegate).
+        class OffscreenEngine
+        {
+        public:
+            OffscreenEngine(Qt3DRender::QCamera *camera, const QSize &size);
+            ~OffscreenEngine();
+
+            void setSceneRoot(Qt3DCore::QNode *sceneRoot);
+            Qt3DRender::QRenderCapture *getRenderCapture();
+            void setSize(const QSize &size);
+
+        private:
+            // We need all of the following in order to render a scene:
+            Qt3DCore::QAspectEngine *aspectEngine;              // The aspect engine, which holds the scene and related aspects.
+            Qt3DRender::QRenderAspect *renderAspect;            // The render aspect, which deals with rendering the scene.
+            Qt3DLogic::QLogicAspect *logicAspect;               // The logic aspect, which runs jobs to do with synchronising frames.
+            Qt3DRender::QRenderSettings *renderSettings;        // The render settings, which control the general rendering behaviour.
+            Qt3DRender::QRenderCapture *renderCapture;          // The render capture node, which is appended to the frame graph.
+            OffscreenSurfaceFrameGraph *offscreenFrameGraph;    // The frame graph, which allows rendering to an offscreen surface.
+            Qt3DCore::QNode *sceneRoot;                         // The scene root, which becomes a child of the engine's root entity.
+        };
     };
 }
 
