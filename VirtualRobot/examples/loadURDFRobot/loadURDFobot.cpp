4bce2380977d23472a3f591baf7dbe3605e721d1
Leslie <marande.leslie@gmail.com>
2019-08-12 16:42:14 +0200

    Pepper like in qibullet

--- VirtualRobot/examples/loadURDFRobot/loadURDFobot.cpp
+++ VirtualRobot/examples/loadURDFRobot/loadURDFobot.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
 
     // atlas file
     // std::string urdfFile = ("robots/urdf/atlas_description/urdf/atlas_v3.urdf");
-    std::string urdfFile = ("robots/urdf/Pepper/urdf/new_qibullet/pepper_rGripper.urdf");
+    std::string urdfFile = ("robots/urdf/Pepper/urdf/qibullet/pepper_rGripper.urdf");
     RuntimeEnvironment::getDataFileAbsolute(urdfFile);
 
     // to ensure that 3d model files can be loaded during converting we need to add the correct data path
